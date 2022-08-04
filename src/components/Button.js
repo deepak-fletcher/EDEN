94c9505e98537c368e9c192e9793c0f425e29035
deepak-fletcher <deepakraofletcher@live.com>
2022-08-04 19:06:08 +0530

    Completed functionalities

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useNavigate } from "react-router-dom";
 const ButtonEden = (props) => {
     let navigate = useNavigate()
+    let [error,setError]=useState(0)
     const handleClick = () =>{
-        navigate(props.nav)
+        if(props.data=="")
+        {
+          setError(1)
+        }
+        else
+        {
+          setError(0)
+          navigate(props.nav)
+        }
+        
     }
   return (
+    <div>
     <div onClick={handleClick} class="flex flex-row">
         <div class="flex-none grow">
         </div>
         <p class="submitButton hover:bg-[#7D7DFF] w-350px md:w-[400px] p-2 hover:cursor-pointer focus:bg-[#7D7DFF]">{props.name}</p>
         <div class="flex-none grow">
+        </div>
+    </div>
+    <div>
+            {error===1 &&
+                <p class="margin-auto pt-1 text-red-600">Please select an option</p>
+            }   
         </div>
     </div>
   )
--- src/components/Usage.js
+++ src/components/Usage.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import {React,useState} from 'react'
 import logo from '../assets/logo.png';
 import solo from '../assets/solo.png';
+import solooff from '../assets/solooff.png';
 import teams from '../assets/teams.png';
+import teamson from '../assets/teamson.png';
 import ButtonEden from './Button';
 import SetupForm from './SetupForm.js';
 import TopComponent from './TopComponent';
 import ProgressBar from './ProgressBar';
 
 const Usage = () => {
+  let [counter,setCounter]=useState(0)
+  let [data, setData]=useState("")
+
+
+
+  const counterToOne=()=>{
+    setCounter(1)
+    setData("Solo")
+  }
+  const counterToTwo=()=>{
+    setCounter(2)
+    setData("Team")
+  }
+
+
+
   return (
     <div class="mb-[5%] mt-[5%]">
     <TopComponent></TopComponent>
@@ -19,17 +37,17 @@ const Usage = () => {
         <p class="text-sm font-inter m-auto font-thin p-5 text-gray-500">We'll streamline your setup experience accordingly.</p>
     </div>
     <div class="mt-[1%]">
-       <div class="flex flex-row w-[350px] md:w-[400px] space-x-[10px] md:space-x-[20px] h-[185px] m-auto">
-            <div class="w-[170px] md:w-[190px] h-[180px] border-[#475AFF] boxes ">
-            <img src={solo} class="mt-[8%] ml-[5%]"></img>
+       <div class="flex flex-row w-[350px] md:w-[400px] space-x-[10px] md:space-x-[20px] h-[185px] m-auto" >
+            <div class={`w-[170px] md:w-[190px] h-[180px] hover:bg-gray-200 boxes ${counter==1? "border-[#475AFF]":"border-[#000000]"}`} onClick={counterToOne}>
+            <img src={counter==1?solo:solooff} class="mt-[8%] ml-[5%]"></img>
             <p class="text-left font-bold pl-5 pt-2">For Myself</p>
             <p class="text-left text-gray-400 pl-5 pt-2">
                 Write better. Think<br></br>
                 more clearly. Stay<br></br> organized.
             </p>
             </div>
-            <div class="w-[170px] md:w-[190px] h-[180px] border-[#000000] boxes">
-            <img src={teams} class="mt-[8%] ml-[5%]"></img>
+            <div class={`w-[170px] md:w-[190px] h-[180px] hover:bg-gray-200 boxes ${counter==2? "border-[#475AFF]":"border-[#000000]"}`} onClick={counterToTwo}>
+            <img src={counter==2?teamson:teams} class="mt-[8%] ml-[5%]"></img>
             <p class="text-left font-bold pl-5 pt-2">With my team</p>
             <p class="text-left text-gray-400 pl-5 pt-2">
                 Wikis, docs, tasks &<br></br>
@@ -38,7 +56,7 @@ const Usage = () => {
             </div>
             
        </div>
-       <ButtonEden nav="/complete" name="Create Workspace"></ButtonEden>
+       <ButtonEden nav="/complete" name="Create Workspace" data={data}></ButtonEden>
     </div>
     
     
