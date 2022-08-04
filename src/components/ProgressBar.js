e186a29bd38735387c710f3239a876fa130b105b
deepak-fletcher <deepakraofletcher@live.com>
2022-08-04 09:07:37 +0530

    progress bar completed

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -6,25 +6,43 @@ const ProgressBar = (props) => {
   return (
     <div class="p-4 flex justify-between items-center">
         <div class="relative flex flex-col items-center text-gray-600">
-            <div class={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex  items-center justify-center ${step>0? "bg-[#475AFF] text-white font-bold":""}`}>1</div>
+            <div class={`rounded-full transition duration-500 ease-in-out  border-gray-300 h-12 w-12 flex  items-center justify-center ${step>0? "bg-[#475AFF] border-1 text-white font-bold":"border-2"}`}>1</div>
         </div>
-        <div class={`flex-auto border-t-2 transition duration-500 ease-in-out ${step>0? "border-[#475AFF]":""}`}>
-
-        </div>
-        <div class="relative flex flex-col items-center text-gray-600">
-            <div class={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex  items-center justify-center ${step>1? "bg-[#475AFF] text-white font-bold":""}`}>2</div>
-        </div>
-        <div class={`flex-auto border-t-2 transition duration-500 ease-in-out ${step>1? "border-[#475AFF]":""}`}>
-
+        <div class={`flex-auto transition duration-500 ease-in-out`}>
+            {step>0 && <span class="flex flex-row"><div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>0? "border-[#475AFF]":""}`}>
+            </div>
+            <div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>1? "border-[#475AFF]":""}`}>
+            </div>
+            </span>}
         </div>
         <div class="relative flex flex-col items-center text-gray-600">
-            <div class={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex  items-center justify-center ${step>2? "bg-[#475AFF] text-white font-bold":""}`}>3</div>
+            <div class={`rounded-full transition duration-500 ease-in-out border-gray-300 h-12 w-12 flex  items-center justify-center ${step>1? "bg-[#475AFF] border-1 text-white font-bold":"border-2"}`}>2</div>
         </div>
-        <div class={`flex-auto border-t-2 transition duration-500 ease-in-out ${step>2? "border-[#475AFF]":""}`}>
-
+        <div class={`flex-auto transition duration-500 ease-in-out`}>
+            { step>1 ? 
+                <span class="flex flex-row"><div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>1? "border-[#475AFF]":""}`}>
+                </div>
+                <div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>2? "border-[#475AFF]":""}`}>
+                </div>
+                </span>: 
+                <span class="flex flex-row"><div class={`flex-auto transition border-t-2 duration-500 ease-in-out`}>
+                </div></span> }
         </div>
         <div class="relative flex flex-col items-center text-gray-600">
-            <div class={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex  items-center justify-center ${step>3? "bg-[#475AFF] text-white font-bold":""}`}>4</div>
+            <div class={`rounded-full transition duration-500 ease-in-out  border-gray-300 h-12 w-12 flex  items-center justify-center ${step>2? "bg-[#475AFF] border-1 text-white font-bold":"border-2"}`}>3</div>
+        </div>
+        <div class={`flex-auto transition duration-500 ease-in-out`}>
+                { step>2 ? 
+                <span class="flex flex-row"><div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>2? "border-[#475AFF]":""}`}>
+                </div>
+                <div class={`flex-auto transition border-t-2 w-1/2 duration-500 ease-in-out ${step>3? "border-[#475AFF]":""}`}>
+                </div>
+                </span>: 
+                <span class="flex flex-row"><div class={`flex-auto transition border-t-2 duration-500 ease-in-out`}>
+                </div></span> }
+        </div>
+        <div class="relative flex flex-col items-center text-gray-600">
+            <div class={`rounded-full transition duration-500 ease-in-out  border-gray-300 h-12 w-12 flex  items-center justify-center ${step>3? "bg-[#475AFF] border-1 text-white font-bold":"border-2"}`}>4</div>
         </div>
         
     </div>
